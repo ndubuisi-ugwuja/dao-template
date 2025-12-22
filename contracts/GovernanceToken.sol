a2dabc52e6e899ea4b7b38132b5edb2ec5564f7c
Ndubuisi <ndujiovita1@gmail.com>
2025-12-22 09:18:01 +0100

    update governance token contract

--- contracts/GovernanceToken.sol
+++ contracts/GovernanceToken.sol
@@ -1,4 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-contract GovernanceToken {}
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+
+abstract contract GovernanceToken is ERC20Votes {
+    uint256 public s_maxSupply = 1000000000000000000000000;
+
+    constructor() ERC20("GovernanceToken", "GT") {
+        _mint(msg.sender, s_maxSupply);
+    }
+}
