8fa4ed2e6f0af6a27c1e09972ddc511152a5ee57
Ndubuisi <ndujiovita1@gmail.com>
2025-12-23 10:46:15 +0100

    add TimeLock contract

--- contracts/governance_standard/TimeLock.sol
+++ contracts/governance_standard/TimeLock.sol
@@ -1,2 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
+
+import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
+
+contract TimeLock is TimelockController {
+    constructor(
+        uint256 minDelay,
+        address[] memory proposers,
+        address[] memory executors,
+        address admin
+    ) TimelockController(minDelay, proposers, executors, admin) {}
+}
