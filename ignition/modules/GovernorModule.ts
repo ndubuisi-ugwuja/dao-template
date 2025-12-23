e8e85f6a6c20140a25309cbcfe7ef4140aad54c7
Ndubuisi <ndujiovita1@gmail.com>
2025-12-23 18:19:18 +0100

    add Governor module file

---
+++ ignition/modules/GovernorModule.ts
@@ -0,0 +1,27 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+import GovernanceTokenModule from "./GovernanceTokenModule";
+import TimeLockModule from "./TimeLockModule";
+
+const GovernorModule = buildModule("GovernorModule", (m) => {
+    // Get deployed GovernanceToken and TimeLock
+    const { governanceToken } = m.useModule(GovernanceTokenModule);
+    const { timeLock } = m.useModule(TimeLockModule);
+
+    // Governor parameters
+    const votingDelay = m.getParameter("votingDelay", 1); // 1 block
+    const votingPeriod = m.getParameter("votingPeriod", 50400); // 1 week (assuming 12s blocks)
+    const quorumPercentage = m.getParameter("quorumPercentage", 4); // 4%
+
+    // Deploy Governor
+    const governor = m.contract("GovernorContract", [
+        governanceToken,
+        timeLock,
+        votingDelay,
+        votingPeriod,
+        quorumPercentage,
+    ]);
+
+    return { governor, governanceToken, timeLock };
+});
+
+export default GovernorModule;
