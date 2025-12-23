79f0cdb4b9670283bd50cf84b817654a54670446
Ndubuisi <ndujiovita1@gmail.com>
2025-12-23 18:43:44 +0100

    fix EIP712 issue

--- contracts/GovernanceToken.sol
+++ contracts/GovernanceToken.sol
@@ -2,11 +2,12 @@
 pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 
-abstract contract GovernanceToken is ERC20Votes {
+contract GovernanceToken is ERC20Votes {
     uint256 public s_maxSupply = 1000000000000000000000000;
 
-    constructor() ERC20("GovernanceToken", "GT") {
+    constructor() ERC20("GovernanceToken", "GT") EIP712("GovernanceToken", "1") {
         _mint(msg.sender, s_maxSupply);
     }
 }
